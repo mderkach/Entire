b2c69fcf131dabab52a22c5028d53df400416e0f
Skeev_ <derkach94@gmail.com>
2018-06-12 22:19:13 +0300

    finish desktop w/o popups

--- app/js/common.js
+++ app/js/common.js
@@ -9,11 +9,26 @@ $(function () {
         prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fas fa-chevron-left'></i></button>",
         nextArrow: "<button type='button' class='slick-next pull-right'><i class='fas fa-chevron-right'></i></button>",
     });
+    ymaps.ready(init);
     var myMap;
-    ymaps.ready(function () {
+    var center = [55.65583971319176,37.611263440505965];
+    if (window.innerWidth <= 580) {
+        center = [55.65583971319176,37.611263440505965];
+    } else {
+        center = [55.65583971319176,37.611263440505965];
+    };
+
+    function init() {
         myMap = new ymaps.Map("map", {
-            center: [55.76, 37.64],
-            zoom: 10
+            center: center,
+            zoom: 17,
         });
-    });
+        var myPlacemark = new ymaps.Placemark([55.6558435691071,37.60730449999999], {}, {
+            preset: 'islands#redIcon'
+        });
+        myMap.geoObjects.add(myPlacemark);
+        if (window.innerWidth <= 580) {
+            myMap.behaviors.disable('drag');
+        };
+    }
 });
