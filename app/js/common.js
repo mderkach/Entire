614b6150610b60785f22f4742ea90575287966a0
Skeev_ <derkach94@gmail.com>
2018-06-13 23:48:23 +0300

    fancybox fix

--- app/js/common.js
+++ app/js/common.js
@@ -11,11 +11,11 @@ $(function () {
     });
     ymaps.ready(init);
     var myMap;
-    var center = [55.65583971319176,37.611263440505965];
+    var center = [55.65583971319176, 37.611263440505965];
     if (window.innerWidth <= 770) {
-        center = [55.65474564549715,37.6071435674591];
+        center = [55.65474564549715, 37.6071435674591];
     } else {
-        center = [55.65583971319176,37.611263440505965];
+        center = [55.65583971319176, 37.611263440505965];
     };
 
     function init() {
@@ -23,7 +23,7 @@ $(function () {
             center: center,
             zoom: 17,
         });
-        var myPlacemark = new ymaps.Placemark([55.6558435691071,37.60730449999999], {}, {
+        var myPlacemark = new ymaps.Placemark([55.6558435691071, 37.60730449999999], {}, {
             preset: 'islands#redIcon'
         });
         myMap.geoObjects.add(myPlacemark);
@@ -32,13 +32,9 @@ $(function () {
         };
     };
     $('input[name="phone"]').inputmask("+7(999)999-99-99");
-    var window_width = $(window).width(),
-        window_height = window.innerHeight,
-        header_height = $(".default-header").height(),
-        header_height_static = $(".site-header.static").outerHeight(),
-        fitscreen = window_height - header_height;
-
-
-    $(".fullscreen").css("height", window_height)
-    $(".fitscreen").css("height", fitscreen);
+    $('[data-fancybox]').fancybox({
+        modal: false,
+        buttons: ["close"],
+        hideScrollbar: true
+    });
 });
